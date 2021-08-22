22442abe9b7f96560adadb0c8c78330dacd2da3d
luisnovaish <luis.novais@engenharia.ufjf.br>
2021-08-22 13:59:39 -0300

    add function to plot the data

--- pytescue.py
+++ pytescue.py
@@ -10,15 +10,21 @@ from matplotlib.widgets import Slider, Button, RadioButtons
 
 def calculate(mag, theta):
 
+    # open data for each phase
     mag_a = mag[0]
     mag_b = mag[1]
     mag_c = mag[2]
 
+    theta_a = theta[0]
+    theta_b = theta[1]
+    theta_c = theta[2]
+
+    # calculate new phasors
     Va = mag_a*np.exp(+1j*( 0      + theta_a))
     Vb = mag_b*np.exp(-1j*(2*pi/3  + theta_b))
     Vc = mag_c*np.exp(+1j*(2*pi/3  + theta_c))
 
-    Vabc = [Va, Vb, Vc]
+    Vabc = [Va, Vb, Vc]      # agregate data
 
     # Define Fortescue Transform
     Vap = K*(Va +   Vb*a    +  Vc*a**2)
@@ -29,14 +35,40 @@ def calculate(mag, theta):
     Vpm0_b = [Vap*a**2, Vam*a, Va0]
     Vpm0_c = [Vap*a, Vam*a**2, Va0]
 
-    Vfortescue = [Vpm0_a,Vpm0_b,Vpm0_c]
+    Vfortescue = [Vpm0_a,Vpm0_b,Vpm0_c]      # agregate data
 
     return Vabc, Vfortescue
 
 
 
-def plots():
-    pass
+def plots(Vabc, Vfortescue, both=True):
+
+    if both:
+        for pm0 in Vfortescue:
+            aux = 0
+            off_x = 0
+            off_y = 0
+
+            for phase_pm0 in pm0:
+                color_p = col_pm0[aux]
+                pm0 = phase_pm0
+                if abs(pm0) > 0.01:
+                    ax.arrow(off_x, off_y, np.real(pm0), np.imag(pm0), color=color_p, length_includes_head=True, width=0.005, head_width=None, head_length=None,overhang=0.6) 
+                    off_x += np.real(pm0)
+                    off_y += np.imag(pm0)
+                aux += 1
+
+
+
+    aux = 0 
+    for abc in Vabc:
+        # plot abc voltage phasors
+        color_a = col_abc[aux]
+        if abs(abc) > 0.01:
+            ax.arrow(0, 0, np.real(abc), np.imag(abc), color=color_a, length_includes_head=True, width=0.005, head_width=None, head_length=None,overhang=0.6) 
+        
+        aux += 1
+
 
 
 ############################################################
@@ -58,17 +90,15 @@ a = 1*np.exp(1j*2*pi/3)
 
 # define module and phase for ABC quantities
 
-theta_a = 0*(20)*pi/180
-theta_b = 0*(60)*pi/180
-theta_c = 0*(120)*pi/180
-
-theta = [theta_a, theta_b, theta_c]
-
 mag_a = 1
 mag_b = 1
 mag_c = 1
-
 mag = [mag_a, mag_b, mag_c]
+
+theta_a = 0*(20)*pi/180
+theta_b = 0*(60)*pi/180
+theta_c = 0*(120)*pi/180
+theta = [theta_a, theta_b, theta_c]
 
 # Fortescue constant (use sqrt(3)/3 for power-invariant transform)
 K = 1/3  
@@ -76,60 +106,15 @@ K = 1/3
 # Call function
 Vabc, Vfortescue = calculate(mag, theta)
 
-# Va = 1*np.exp(+1j*( 0      + theta_a))
-# Vb = 1*np.exp(-1j*(2*pi/3  + theta_b))
-# Vc = 1*np.exp(+1j*(2*pi/3  + theta_c))
-
-# Vabc = [Va, Vb, Vc]
-
-# # Define Fortescue Transform
-# Vap = K*(Va +   Vb*a    +  Vc*a**2)
-# Vam = K*(Va +   Vb*a**2 +  Vc*a)
-# Va0 = K*(Va +   Vb      +  Vc)
-
-# Vpm0_a = [Vap, Vam, Va0]
-# Vpm0_b = [Vap*a**2, Vam*a, Va0]
-# Vpm0_c = [Vap*a, Vam*a**2, Va0]
-
-# Vfortescue = [Vpm0_a,Vpm0_b,Vpm0_c]
-
-
-
 # Plot stuff
 fig, ax = plt.subplots()
-# ax.set_xlim([-1.2,1.2])
-# ax.set_ylim([-1,1])
 ax.set(xlim=lim_x, ylim=lim_y)
 ax.set_box_aspect(1)
 ax.grid(ls=':')
 
-aux = 0 
-for abc in Vabc:
 
-    # plot abc voltage phasors
-    color_a = col_abc[aux]
-    if abs(abc) > 0.01:
-        ax.arrow(0, 0, np.real(abc), np.imag(abc), color=color_a, length_includes_head=True, width=0.005, head_width=None, head_length=None,overhang=0.6) 
-    
-    aux += 1
-
-# # plot +, -, 0 phasors
-# aux = 0
-# off_x = 0
-# off_y = 0
-
-# # plt.show()
-
-# for pm0 in Vfor:
-#     color_p = col_pm0[aux]
-#     pm0 = Vfor[aux]
-#     if abs(abc) > 0.01:
-#         ax.arrow(off_x, off_y, np.real(pm0), np.imag(pm0), color=color_p, length_includes_head=True, width=0.005, head_width=None, head_length=None,overhang=0.6) 
-#         off_x += np.real(pm0)
-#         off_y += np.imag(pm0)
-#     aux += 1
-
-
+plots(Vabc, Vfortescue, both=False)
+        
 ###### Slider data
 # Magnitude limits
 mag_ini = 1                        # initial frequency plot
@@ -203,54 +188,19 @@ def update(val):
 
     theta_c = slid_theta_c.val*pi/180
     mag_c = slid_mag_c.val
+
+    # update vector with new data
+    theta = [theta_a, theta_b, theta_c]
+    mag = [mag_a, mag_b, mag_c]
     
     # call function
-    Va = mag_a*np.exp(+1j*( 0      + theta_a))
-    Vb = mag_b*np.exp(-1j*(2*pi/3  + theta_b))
-    Vc = mag_c*np.exp(+1j*(2*pi/3  + theta_c))
-
-    Vabc = [Va, Vb, Vc]
-
-    Vap = K*(Va +   Vb*a    +  Vc*a**2)
-    Vam = K*(Va +   Vb*a**2 +  Vc*a)
-    Va0 = K*(Va +   Vb      +  Vc)
-
-    Vpm0_a = [Vap, Vam, Va0]
-    Vpm0_b = [Vap*a**2, Vam*a, Va0]
-    Vpm0_c = [Vap*a, Vam*a**2, Va0]
-
-    Vfortescue = [Vpm0_a,Vpm0_b,Vpm0_c]
-
+    Vabc, Vfortescue = calculate(mag, theta)
 
     ax.cla()
     ax.grid(ls=':')
     ax.set(xlim=lim_x, ylim=lim_y)
 
-    for pm0 in Vfortescue:
-        aux = 0
-        off_x = 0
-        off_y = 0
-
-        for phase_pm0 in pm0:
-            color_p = col_pm0[aux]
-            pm0 = phase_pm0
-            if abs(pm0) > 0.01:
-                ax.arrow(off_x, off_y, np.real(pm0), np.imag(pm0), color=color_p, length_includes_head=True, width=0.005, head_width=None, head_length=None,overhang=0.6) 
-                off_x += np.real(pm0)
-                off_y += np.imag(pm0)
-            aux += 1
-
-
-
-    aux = 0 
-    for abc in Vabc:
-        # plot abc voltage phasors
-        color_a = col_abc[aux]
-        if abs(abc) > 0.01:
-            ax.arrow(0, 0, np.real(abc), np.imag(abc), color=color_a, length_includes_head=True, width=0.005, head_width=None, head_length=None,overhang=0.6) 
-        
-        aux += 1
-
+    plots(Vabc, Vfortescue)
         
     fig.canvas.draw_idle()
 
